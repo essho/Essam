d319058db72be97152a7030b732442d8c20b51cd
essho <essho@yahoo.com>
2025-08-26 17:37:00 +0300

    dl

--- utils.js
+++
@@ -1,6 +0,0 @@
-// utils.js: يحتوي على دوال مساعدة صغيرة يمكن استخدامها في أي مكان.
-
-export function hexToRgb(hex) {
-    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? { r: parseInt(result[1], 16), g: parseInt(result[2], 16), b: parseInt(result[3], 16) } : null;
-}
